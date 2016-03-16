a95b971cd601624c4e5ead3908568bd74a959d14
dragonly <liyilongko@gmail.com>
2016-03-16 21:01:37 +0800

    make clear

--- examples/src/main/scala/SparkAqlExample.scala
+++ examples/src/main/scala/SparkAqlExample.scala
@@ -6,6 +6,8 @@ import org.fudan.SparkAql.aql._
 object SparkAqlExample{
   def main(args: Array[String]) {
     val hi = Hi
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val sc = new SparkContext(conf)
     hi.hi
   }
 }
