cdb20b529e1bd7ae8e7e8593c29b77d18334135e
scgump <14210240091@fudan.edu.cn>
2016-03-18 16:06:21 +0800

    amendation

--- aql/src/main/scala/SparkAql/aql/plan/logical/Unresolved.scala
+++ aql/src/main/scala/SparkAql/aql/plan/logical/Unresolved.scala
@@ -1,6 +1,5 @@
 package SparkAql.aql.plan.logical
 
-
 case class UnresolvedDictView(DictName: String, ViewName: String) extends LeafNode{
   override def toString = "A new View named "+ViewName+" extracted from document with dictionary " + DictName
 }
